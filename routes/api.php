f3b83b470ce27b047bd38c67ae69107f956f9e1e
Jetulio <joo_ramos2006@hotmail.com>
2026-03-16 18:01:48 -0300

    chore: change on product routes and treated user show

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,17 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+    return UserResource::make($request->user());
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('/product', ProductController::class)->only('index', 'store');
+    Route::get("/products/{product}", [ProductController::class, 'show'])->name('product.show');
+    Route::apiResource('/products', ProductController::class)->only('index', 'show', 'store', 'update', 'destroy');
 });
 
 require __DIR__ . '/auth.php';
