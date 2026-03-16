73987d246772210369c5247bba9cf623518c5491
Jetulio <joo_ramos2006@hotmail.com>
2026-03-16 14:55:19 -0300

    chore: ajusted tests for register method

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -9,16 +10,87 @@ class RegistrationTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_new_users_can_register(): void
+    public function test_admin_users_can_register_other_users(): void
     {
-        $response = $this->post('/api/register', [
+        $user = User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+            'role' => 'ADMIN',
+        ]);
+
+        $response = $this->actingAs($user)->post('/api/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
+            'role' => 'USER',
         ]);
 
-        $this->assertAuthenticated();
+        expect(User::where('email', 'test@example.com')->exists())->toBeTrue();
         $response->assertNoContent();
+    }
+
+    public function test_manager_users_can_register_other_users(): void
+    {
+        $user = User::create([
+            'name' => 'Manager User',
+            'email' => 'manager@admin.com',
+            'password' => bcrypt('password'),
+            'role' => 'ADMIN',
+        ]);
+
+        $response = $this->actingAs($user)->post('/api/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'role' => 'USER',
+        ]);
+
+        expect(User::where('email', 'test@example.com')->exists())->toBeTrue();
+        $response->assertNoContent();
+    }
+
+    public function test_users_cant_register_other_users(): void
+    {
+        $user = User::create([
+            'name' => 'Manager User',
+            'email' => 'manager@admin.com',
+            'password' => bcrypt('password'),
+            'role' => 'USER',
+        ]);
+
+        $response = $this->actingAs($user)->post('/api/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'role' => 'USER',
+        ]);
+
+        expect(User::where('email', 'test@example.com')->exists())->toBeFalse();
+        $response->assertStatus(403);
+    }
+
+    public function test_finance_cant_register_other_users(): void
+    {
+        $user = User::create([
+            'name' => 'Manager User',
+            'email' => 'manager@admin.com',
+            'password' => bcrypt('password'),
+            'role' => 'FINANCE',
+        ]);
+
+        $response = $this->actingAs($user)->post('/api/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'role' => 'USER',
+        ]);
+
+        expect(User::where('email', 'test@example.com')->exists())->toBeFalse();
+        $response->assertStatus(403);
     }
 }
