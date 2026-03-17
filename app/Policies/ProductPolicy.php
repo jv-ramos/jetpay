64a869ec9fa8e9af835faf1bf10b7dc19ae5ad49
Jetulio <joo_ramos2006@hotmail.com>
2026-03-17 09:36:18 -0300

    chore: updated product policies

--- app/Policies/ProductPolicy.php
+++ app/Policies/ProductPolicy.php
@@ -11,18 +11,18 @@ class ProductPolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
-    {
-        return false;
-    }
+    // public function viewAny(User $user): bool
+    // {
+    //     return false;
+    // }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Product $product): bool
-    {
-        return false;
-    }
+    // public function view(User $user, Product $product): bool
+    // {
+    //     return false;
+    // }
 
     /**
      * Determine whether the user can create models.
@@ -51,16 +51,16 @@ class ProductPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Product $product): bool
-    {
-        return false;
-    }
+    // public function restore(User $user, Product $product): bool
+    // {
+    //     return false;
+    // }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Product $product): bool
-    {
-        return false;
-    }
+    // public function forceDelete(User $user, Product $product): bool
+    // {
+    //     return false;
+    // }
 }
