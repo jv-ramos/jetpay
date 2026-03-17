19a2cb3c090fe7bd8b4a42f6afe3818cefe80296
Jetulio <joo_ramos2006@hotmail.com>
2026-03-17 08:55:07 -0300

    feat: add missing user routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,17 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return UserResource::make($request->user());
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/user', UserController::class)->only('update', 'destroy');
 });
 
 Route::middleware('auth:sanctum')->group(function () {
