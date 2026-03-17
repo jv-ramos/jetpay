c9f514f9a13a3cda5271fa952e9009a993c34c72
Jetulio <joo_ramos2006@hotmail.com>
2026-03-17 11:45:53 -0300

    feat: add client tests

---
+++ tests/Feature/Client/ClientTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Client;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+describe('Client', function () {
+
+    uses(RefreshDatabase::class);
+
+    it('should create a Client sucessfully', function () {
+        $client = Client::create(['name' => 'client_test', 'email' => 'client@example.com']);
+
+        expect($client->exists())->toBeTrue();
+    });
+
+    it('should fail to create a Client without a name', function () {
+        expect(fn() => Client::create(['email' => 'client@example.com']))->toThrow(\Illuminate\Database\QueryException::class);
+    });
+
+    it('should fail to create a Client with an email already taken', function () {
+        Client::create(['name' => 'client_test', 'email' => 'client@example.com']);
+        expect(fn() => Client::create(['name' => 'client_test2', 'email' => 'client@example.com']))
+            ->toThrow(\Illuminate\Database\QueryException::class);
+    });
+});
