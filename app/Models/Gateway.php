0ecf93f892190b17b5e4da9a7e6ade4f1f1be577
Jetulio <joo_ramos2006@hotmail.com>
2026-03-17 18:08:40 -0300

    add gateway model

---
+++ app/Models/Gateway.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Gateway extends Model
+{
+    protected $fillable = [
+        'name',
+        'is_active',
+        'priority',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+}
