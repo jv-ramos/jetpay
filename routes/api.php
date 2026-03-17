f11052e49e135a60c0c76bca806d27ba32a6242f
Jetulio <joo_ramos2006@hotmail.com>
2026-03-17 11:47:03 -0300

    feat: add client index route

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,21 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+/*
+/ CLIENT ROUTES
+*/
+
+Route::get('/clients', ClientController::class);
+
+/*
+/ USER ROUTES
+*/
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return UserResource::make($request->user());
@@ -13,6 +24,10 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/user', UserController::class)->only('update', 'destroy');
 });
+
+/*
+/ PRODUCT ROUTES
+*/
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get("/products/{product}", [ProductController::class, 'show'])->name('product.show');
