dd299e76c9b478bde5a141c799315fc43f409d2e
Jetulio <joo_ramos2006@hotmail.com>
2026-03-30 16:03:44 -0300

    chore: update on client policy

--- app/Policies/ClientPolicy.php
+++ app/Policies/ClientPolicy.php
@@ -51,16 +51,16 @@ class ClientPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Client $client): bool
-    {
-        return false;
-    }
+    // public function restore(User $user, Client $client): bool
+    // {
+    //     return false;
+    // }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Client $client): bool
-    {
-        return false;
-    }
+    // public function forceDelete(User $user, Client $client): bool
+    // {
+    //     return false;
+    // }
 }
